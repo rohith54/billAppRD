beb0c697560254c7883859226930289df3491cbc
Rohith <Rohith@LAPTOP-UJCCV563>
2021-06-13 20:34:43 +0530

    First committ

---
+++ src/main/java/com/rohith/billAppRD/BillAppRdApplication.java
@@ -0,0 +1,13 @@
+package com.rohith.billAppRD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillAppRdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillAppRdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohith/billAppRD/BillAppRdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohith.billAppRD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillAppRdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
